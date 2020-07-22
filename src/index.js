5e51821ef0b7c69bdab678bb56c27e648f542917
helia <66224930+HeliaRasooli@users.noreply.github.com>
2020-07-22 18:48:37 +0430

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,34 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Second from './Second';
 import * as serviceWorker from './serviceWorker';
+import { Container } from '@material-ui/core';
+import Controller from './Controller';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Controller />
   </React.StrictMode>,
   document.getElementById('root')
 );
-const input = document.getElementById("search-input");
-const searchBtn = document.getElementById("search-btn");
-
-const expand = () => {
-  searchBtn.classList.toggle("close");
-  input.classList.toggle("square");
-};
-
-searchBtn.addEventListener("click", expand);
-
-const defaultProps = {
-  bgcolor: 'background.paper',
-  m: 1,
-  border: 1,
-  style: { width: '5rem', height: '5rem' },
-};
 
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
