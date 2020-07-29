5707bfbc02b7022f9e09520926bd2a28b27b013e
Adham Saadi <49848447+adhamsaadi@users.noreply.github.com>
2020-07-29 17:28:42 +0430

    Update views.py

--- jangoApp/views.py
+++ jangoApp/views.py
@@ -1,6 +1,25 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.http import HttpResponse, HttpResponseNotFound
+import datetime
+from rest_framework import views, serializers, status
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 
-from django.shortcuts import render
 
-# Create your views here.
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = '<html><body>It is now %s.</body></html>' % now
+
+    return HttpResponseNotFound(html)
+
+
+class RegisterSeializer(serializers.Serializer):
+    name = serializers.CharField(max_length=64)
+    email = serializers.EmailField()
+    password  = serializers.CharField(max_length=16, min_length=6)
+
+class RegisterView(views.APIView):
+    permission_classes = (AllowAny,)
+    def post(self, request):
+        serializer = RegisterSeializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
